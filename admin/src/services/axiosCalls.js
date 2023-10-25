267a327091ad3e33235c7dcb4312fa4363cbc403
laliteshx6 <2200031554@kluniversity.in>
2023-10-25 16:12:45 +0530

    added autentication

---
+++ admin/src/services/axiosCalls.js
@@ -0,0 +1,13 @@
+import apis from "./Apis";
+import axios from 'axios';
+export const Get = async (url, options) => {
+  console.log('get api call')
+  return (await axios.get(url, options));
+}
+ export const Post =(p)=>{
+  return axios({
+      baseURL : apis.BASE,
+      method:'post',
+      ...p,
+  })
+ }
